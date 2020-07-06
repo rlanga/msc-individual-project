5d9b7d7ae3ea807afb33a35eb82c2114662f65df
Roy Langa <roy.langa@gmail.com>
2020-07-06 18:56:26 +0100

    Added finger fix and predecessor check interval values in node state

--- lib/structs/node_state.ex
+++ lib/structs/node_state.ex
@@ -6,5 +6,7 @@ defmodule NodeState do
             finger: [],
             keys: [],
             stabilization_interval: 30 * 1000, # 30 seconds
+            finger_fix_interval: 30 * 1000, # 30 seconds
+            predecessor_check_interval: 30 * 1000, # 30 seconds
             bit_size: 160 # Default is 160 bits as that's the size of SHA-1
 end
