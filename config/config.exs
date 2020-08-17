5bc8ae437aa03c11c8f9c8d9d1bcb0cade50028d
Roy Langa <roy.langa@gmail.com>
2020-08-17 17:47:46 +0100

    Finished implementation section. Started how to use section

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,11 @@ import Config
 config :chord,
   simulation: true,
   network_size: 32
+  # port: 12543,
+  # address: localhost,
+  # id: "Node1",
+  # join_id: "3",
+  # join_address: "https://node2.othernetwork.com/47382",
 
 config :logger,
   level: :warn
